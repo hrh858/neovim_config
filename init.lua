8bf0c4067c7e1f3b5646b97e1192a46aba930b10
hrh858 <jgimenezp99@outlook.com>
2025-01-07 16:34:12 +0900

    Some cahnges

--- init.lua
+++ init.lua
@@ -1,9 +1,14 @@
-require("config.lazy")
-require("telescope").setup()
-require("mason").setup()
-
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.fillchars:append({ eob = " " })
+
+
+require("config.lazy")
+require("telescope").setup({})
+require("mason").setup()
+
+vim.cmd [[colorscheme tokyonight-night]]
